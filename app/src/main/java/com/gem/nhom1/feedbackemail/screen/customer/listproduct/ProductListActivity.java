b475109a67b83a6e70f2bcfbf2157bdad20a3537
Nghicv <nghicv.57@gmail.com>
2016-02-26 09:35:41 +0700

    fix ui

--- app/src/main/java/com/gem/nhom1/feedbackemail/screen/customer/listproduct/ProductListActivity.java
+++ app/src/main/java/com/gem/nhom1/feedbackemail/screen/customer/listproduct/ProductListActivity.java
@@ -4,13 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.Adapter;
 import android.widget.ListView;
 
 import com.gem.nhom1.feedbackemail.base.BaseActivity;
 import com.gem.nhom1.feedbackemail.base.BasePresenter;
 import com.gem.nhom1.feedbackemail.network.entities.Dealer;
+import com.gem.nhom1.feedbackemail.network.entities.Unit;
+import com.gem.nhom1.feedbackemail.network.entities.UnitPrice;
 import com.project.gem.feedbackemail.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 
@@ -19,10 +26,15 @@ import butterknife.Bind;
  */
 public class ProductListActivity extends BaseActivity<ProductListPresenter> implements ProductListView{
 
-    private Dealer dealer = new Dealer();
+    private Dealer dealer;
+    private List<UnitPrice> unitPrices = new ArrayList<>();
+    private ProductListAdapter adapter;
 
     @Bind(R.id.lv_product)
     ListView lvProduct;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected int getLayoutId() {
@@ -40,17 +52,23 @@ public class ProductListActivity extends BaseActivity<ProductListPresenter> impl
     }
 
     @Override
-    public void onLoadProductListSuccess(ProductListAdapter adapter) {
-        lvProduct.setAdapter(adapter);
+    public void onLoadProductListSuccess(List<UnitPrice> unitPrices) {
+        this.unitPrices.addAll(unitPrices);
+        adapter.notifyDataSetChanged();
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setSupportActionBar(toolbar);
         Intent i = getIntent();
         Bundle bundle = i.getExtras();
         dealer = (Dealer)bundle.getSerializable("dealer");
         Log.d("nghicv", dealer.getName());
+
+        adapter = new ProductListAdapter(getBaseContext(), unitPrices);
+        lvProduct.setAdapter(adapter);
 
         getPresenter().doLoadListProduct(dealer);
 
--- app/src/main/java/com/gem/nhom1/feedbackemail/screen/customer/listproduct/ProductListView.java
+++ app/src/main/java/com/gem/nhom1/feedbackemail/screen/customer/listproduct/ProductListView.java
@@ -1,10 +1,14 @@
 package com.gem.nhom1.feedbackemail.screen.customer.listproduct;
 
 import com.gem.nhom1.feedbackemail.base.BaseView;
+import com.gem.nhom1.feedbackemail.network.entities.Unit;
+import com.gem.nhom1.feedbackemail.network.entities.UnitPrice;
+
+import java.util.List;
 
 /**
  * Created by nghicv on 25/02/2016.
  */
 public interface ProductListView extends BaseView<ProductListPresenter> {
-    public void onLoadProductListSuccess(ProductListAdapter adapter);
+    public void onLoadProductListSuccess(List<UnitPrice> unitPrices);
 }
